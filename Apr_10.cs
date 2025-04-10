e266c04a49b2e858b9aa9014e001c483352dfae7
RomelA77 <romelzekep439@stemcollegiate.ca>
2025-04-10 10:17:15 -0600

    Update Apr_10.cs

--- Apr_10.cs
+++ Apr_10.cs
@@ -1,2 +1,5 @@
 // Module 5 unit 1-2
-// I learned how to do simple addition and multiplication in C#.
+// I learned how to do simple addition, and also learned how to change it into simple multiplication.
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
